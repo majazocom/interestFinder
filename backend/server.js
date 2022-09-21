b363520e34ccdeffd3379d0fb75eb525e6dabc9a
majazocom <maja.gedda@zocom.se>
2022-09-21 11:04:46 +0200

    a start

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const nedb = require('nedb-promise');
+const cors = require('cors');
+
+//skapa konto
+//logga in
+//lägga till intressen
+//hitta matchande intressen
