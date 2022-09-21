fa0b849e0949458b4c64e02a6d207a99b5870bca
majazocom <maja.gedda@zocom.se>
2022-09-21 14:27:36 +0200

    login done

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ const cors = require('cors');
 const app = express();
 
 //säg till servern att alla får utnyttja vårt api
-app.use(cors({origin: '*'}));
+app.use(cors({ origin: '*' }));
+//eftersom vi kommer att skicka å ta emot json-data
 app.use(express.json());
 
 //en databas för konton
-const accountsDB = new nedb({filename: 'accounts.db', autoload: true});
+const accountsDB = new nedb({ filename: 'accounts.db', autoload: true });
 //en databas för intressen länkade till ett konto
-const interestsDB = new nedb({filename: 'interests.db', autoload: true});
+const interestsDB = new nedb({ filename: 'interests.db', autoload: true });
 
 //skapa konto - endpoint
 app.post('/signup', async (request, response) => {
@@ -24,10 +25,10 @@ app.post('/signup', async (request, response) => {
     //kolla igenom db om namnet som skickats in redan existerar
     const usernameExists = await accountsDB.find({ username: credentials.username });
     //.find() returnerar en lista på alla träffar
-    if ( usernameExists.length > 0) {
+    if (usernameExists.length > 0) {
         responseObject.usernameExists = true
     }
-    if (usernameExists) {
+    if (responseObject.usernameExists) {
         responseObject.success = false
     } else {
         //om anv.namn är unikt och allt är fint - tjoffa in i db
@@ -37,6 +38,28 @@ app.post('/signup', async (request, response) => {
     response.json(responseObject);
 });
 //logga in
+app.post('/login', async (request, response) => {
+    const credentials = request.body;
+    const responseObject = {
+        success: true,
+        user: '',
+        interests: []
+    };
+    console.log(credentials);
+    //kollar mot accountsDB om användare med namnet finns
+    const account = await accountsDB.find({ $and: [{ username: credentials.username }, { password: credentials.password }] });
+    if (account.length > 0) {
+        //om vi fick iaf ett konto med träff
+        //vill vi sedan checka lösenordet
+        console.log('korrekt lösen ', account[0]);
+        responseObject.user = account[0].username;
+        //vi vill kolla om användaren har intressen
+        //om det finns; skicka med i responseobj
+    } else {
+        responseObject.success = false;
+    }
+    response.json(responseObject);
+});
 //lägga till intressen
 //hitta matchande intressen
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,62 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+  const navigate = useNavigate();
+
+  async function signup() {
+    const account: object = {
+      username: username,
+      password: password
+    };
+    const response = await fetch('http://localhost:1234/signup', {
+      method: 'POST',
+      body: JSON.stringify(account),
+      headers: { "Content-Type": "application/json" }
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+  async function login() {
+    const account: object = {
+      username: loginUsername,
+      password: loginPassword
+    };
+    const response = await fetch('http://localhost:1234/login', {
+      method: 'POST',
+      body: JSON.stringify(account),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    const data = await response.json();
+    console.log(data);
+    navigate('/start');
+  }
+
   return (
     <div className="App">
-      <h1>Sign up / Log in</h1>
+      <h1>InterestFinder</h1>
+      <section>
+        <h2>Sign up</h2>
+        <label htmlFor='username'>User name</label>
+        <input onChange={(e) => setUsername(e.target.value)} id="username" type="text" placeholder="Username"></input>
+        <label htmlFor='password'>Password</label>
+        <input onChange={(e) => setPassword(e.target.value)} id="password" type="password"></input>
+        <button onClick={signup}>Skapa konto</button>
+      </section>
+      <section>
+        <h2>Log in</h2>
+        <label htmlFor='login-username'>User name</label>
+        <input onChange={(e) => setLoginUsername(e.target.value)} id="login-username" type="text" placeholder="Username"></input>
+        <label htmlFor='login-password'>Password</label>
+        <input onChange={(e) => setLoginPassword(e.target.value)} id="login-password" type="password"></input>
+        <button onClick={login}>Logga in</button>
+      </section>
     </div>
   );
 }
